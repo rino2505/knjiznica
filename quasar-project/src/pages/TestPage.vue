d2da67f8dfacd11bc593ecc31a6a24bdecaaddd6
rino2505 <rinovisic@gmail.com>
2025-11-03 11:27:40 +0100

    nove izmjene

--- quasar-project/src/pages/TestPage.vue
+++ quasar-project/src/pages/TestPage.vue
@@ -1,7 +1,9 @@
 <template>
   <q-page padding>
     <!-- content -->
-
+    <a :href="link.url" :target="link.target">{{ link.text }}</a>
+    <q-input color="green" v-model="text1" label="Item name" @keyup="log()"> </q-input>
+    <q-input color="red" v-model="text1" label="Naziv" @keyup="log()"></q-input>
     <h1>{{ porukica }}</h1>
     <p v-html="poruka"></p>
     <p v-html="htmlText"></p>
@@ -14,13 +16,25 @@
         {{ item }}
       </button>
     </p>
-
   </q-page>
 </template>
 
 <script setup>
-const porukica="Guten tag, wie geht es Ihnen?"
-const poruka='Danke, gut. UND IHNEN?'
-const htmlText="<p><b>Formatirani tekst </b></p>"
-const days=["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak","Subota","Nedjelja"]
+  import {ref} from 'vue'
+  const text1=ref('')
+  const text2=ref('')
+  text1.value='Hello'
+  const link ={
+    url:'https://google.com',
+    target:'_blank',
+    text:'Google'
+  };
+  const porukica="Guten tag, wie geht es Ihnen?"
+  const poruka='Danke, gut. UND IHNEN?'
+  const htmlText="<p><b>Formatirani tekst </b></p>"
+  const days=["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak","Subota","Nedjelja"]
+  function log(){
+    console.log(text1.value)
+    console.log(text2.value)
+  }
 </script>
